2e203bdc694c9f1d5327e76357dd61ee2ea5927a
Sam Minns <sambminns@gmail.com>
2014-11-18 10:07:30 +1300

    Removing unnecessary classes

--- lib/graph_edge.rb
+++
@@ -1,6 +0,0 @@
-class GraphEdge
-  def initialize(origin_node, destination_node)
-    @origin_node      = origin_node
-    @destination_node = destination_node
-  end
-end
--- lib/graph_node.rb
+++ lib/graph_node.rb
@@ -2,8 +2,4 @@ class GraphNode
   def initialize
     @edges_from_node = []  
   end
-
-  def add_edge_from_node(edge)
-    @edges_from_node.push(edge) unless @edges_from_node.include?(edge)
-  end
 end
